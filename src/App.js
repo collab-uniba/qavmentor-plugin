4e1c05fbe5beb77a91476f3863fb194c81b869d4
gigium <gigiM1995@gmail.com>
2018-06-04 12:44:48 +0200

    add material-ui integration for better UI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { withStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
+
+
 
 import './App.css';
 
 class App extends Component
 {
   constructor(props) {
+    console.log('constructor')
     super(props);
-
-    this.handleMouseHover = this.handleMouseHover.bind(this);
+    this.timer = null;
     this.state = {
       "attached_to": props.attached_to,
-      "prediction": "-"
+      "my_style": props.my_style,
+      "prediction": props.percentage
     };
 
-    var html_question = document.getElementById('wmd-preview')
-    var title = document.getElementById('title');
-    var tag = document.getElementById('tagnames');
-    this.interval = setInterval(() => {
+    this.request =   function()
+    {
+      console.log('making request from '+ this.props.attached_to)
+      var html_question = document.getElementById('wmd-preview')
+      var title = document.getElementById('title');
+      var tag = document.getElementById('tagnames');
+
+
       var html_question_inner = ''
       if(html_question)
         html_question_inner = html_question.innerHTML
       else
         html_question = document.getElementById('wmd-preview')
       var date = new Date()
+
+
+      this.setState({
+        prediction: -1
+      })
 
       axios.post('http://127.0.0.1:5000/analyze',
       {
@@ -34,49 +49,34 @@ class App extends Component
           "tags": tag.value.split(" ")
       })
         .then( (response) => {
-          this.setState({
+            console.log("RESPONSE> "+ response.data.prediction)
+            this.setState({
               prediction: parseInt(parseFloat(response.data.prediction)*100)
-          });
+            })
         })
         .catch( (error) => {
-          // console.log(error);
+          console.log('request failed, trying again ... ')
+          this.request()
         });
-    }, 500);
+    }
+
   }
 
 
-    handleMouseHover(){
-      this.setState(this.toggleHoverState);
-    }
-
-    toggleHoverState(state) {
-      return {
-        isHovering: !state.isHovering,
-      };
-    }
-    //
-    // CodeSnippet: { this.state.CodeSnippet }, BodyLength: { this.state.BodyLength }, TitleLength: { this.state.TitleLength },
-    //                        SentimentScore: { this.state.SentimentScore }, Ntag: { this.state.Ntag }, AvgUpperCharsPPost: { this.state.AvgUpperCharsPPost },
-    //                        url: { this.state.url }, WeekDay: {this.state.Weekday}, DayTime: {this.state.GMTHour}
-
-    // {this.state.isHovering && <div>CodeSnippet: { this.state.CodeSnippet }, BodyLength: { this.state.BodyLength }, TitleLength: { this.state.TitleLength },
-    //                        SentimentScore: { this.state.SentimentScore }, Ntag: { this.state.Ntag }, AvgUpperCharsPPost: { this.state.AvgUpperCharsPPost },
-    //                        url: { this.state.url }, WeekDay: {this.state.Weekday}, DayTime: {this.state.GMTHour} </div>}
-
-    // {!this.state.isHovering && <div>{ this.state.prediction }</div>}
 
 
   render() {
+    var prediction_div
+    if(this.state.prediction === -1 || !this.state.prediction)
+      prediction_div = (<div><CircularProgress size={30} /><div className="percentage"></div></div>)
+    else
+      prediction_div = (<div><CircularProgress variant="static" value={ this.state.prediction } size={30} /><div className="percentage">{ this.state.prediction }</div></div>)
 
     return (
-      <div className={"plugin_div_" +this.state.attached_to }
-                       onMouseEnter={this.handleMouseHover}
-                       onMouseLeave={this.handleMouseHover}>
+      <div className={"plugin_div_" +this.state.my_style }>
 
-
-        <div className="round_button" href="">
-          { this.state.prediction }
-
+        <div>
+            {prediction_div}
         </div>
       </div>
 
@@ -84,21 +84,37 @@ class App extends Component
   }
 
 
+
+
   componentDidMount() {
+    console.log(this.props.attached_to + "  mounted")
+    this.request()
 
+    var mounted_on = this.props.attached_to
+    var att = document.getElementById(mounted_on)
+    att.onkeydown = function()
+      {
+        console.log("___________keydown on "+mounted_on+"___________")
+        clearTimeout(this.timer);
+        this.timer = setTimeout(function()
+        {
+            this.request()
 
-
-    // setInterval( () => {
-    //
-    //
-    // },1000)
+        }.bind(this), 500)
+      }.bind(this)
   }
 
 
   componentWillUnmount(){
-    console.log("clearing interval")
-    clearInterval(this.interval)
+    console.log("unmounting "+ this.props.attached_to)
+    clearInterval(this.timer)
   }
+
+
+
+
+
+
 
 }
 
--- src/index.js
+++ src/index.js
@@ -2,28 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import axios from 'axios';
 
-// Get the element to prepend our app to from https://www.google.com.
-// This could be a specific element on a website or something more general like `document.body`.
+
+
+// var percentage = -1
+
 const question_body = document.getElementById('wmd-input');
 const title = document.getElementById('title');
 
-// Create a div to render the App component to.
-const app = document.createElement('div');
+const plugin_button = document.createElement('div');
 
-// Set the app element's id to `root`.
-// This name is the same as the element that create-react-app renders to by default
-// so it will work on the development server too.
-app.id = 'root';
+plugin_button.id = 'root';
 
-// Prepend the App to the viewport element in production if it exists on the page.
-// You could also use `appendChild` depending on your needs.
 
-// Render the App.
+// ReactDOM.render(<App attached_to="tags" my_style="tags" percentage={percentage}/> , document.getElementById('root'));
+
+
 question_body.onfocus = function()
 {
-  question_body.parentNode.insertBefore(app, question_body.nextSibling);
-  ReactDOM.render(<App attached_to="body"/>, document.getElementById('root'));
+  // make_request()
+  question_body.parentNode.insertBefore(plugin_button, question_body.nextSibling);
+  ReactDOM.render(<App attached_to="wmd-input" my_style="body"/>, document.getElementById('root'));
 }
 
 question_body.onblur = function()
@@ -35,8 +35,13 @@ question_body.onblur = function()
 
 title.onfocus = function()
 {
-  title.parentNode.insertBefore(app, title.nextSibling);
-  ReactDOM.render(<App attached_to="title"/>, document.getElementById('root'));
+  // make_request()
+  title.parentNode.insertBefore(plugin_button, title.nextSibling);
+  if(document.getElementById('question-suggestions').childNodes.length == 1)
+    ReactDOM.render(<App attached_to="title" my_style="title" /> , document.getElementById('root'));
+  else
+    ReactDOM.render(<App attached_to="title" my_style="title_suggestion" /> , document.getElementById('root'));
+
 }
 
 title.onblur = function()
@@ -44,3 +49,74 @@ title.onblur = function()
   console.log("unmounting title")
   ReactDOM.unmountComponentAtNode(document.getElementById('root'));
 }
+
+
+
+
+
+
+
+window.onload = function() {
+  // make_request()
+
+  const tags = document.getElementsByClassName("tag-editor")[0].childNodes[1]
+  tags.id = 'tags'
+
+
+  tags.onfocus = function()
+  {
+    // make_request()
+    tags.parentNode.insertBefore(plugin_button, tags.nextSibling);
+    if(document.getElementById('question-suggestions').childNodes.length == 1)
+      ReactDOM.render(<App attached_to="tags" my_style="tags" /> , document.getElementById('root'));
+    else
+      ReactDOM.render(<App attached_to="tags" my_style="tags" /> , document.getElementById('root'));
+
+  }
+
+  tags.onblur = function()
+  {
+    console.log("unmounting tags")
+    ReactDOM.unmountComponentAtNode(document.getElementById('root'));
+  }
+
+};
+
+
+
+
+
+//
+//
+// var make_request = function()
+// {
+//   console.log('making request from index')
+//   var html_question = document.getElementById('wmd-preview')
+//   var title = document.getElementById('title');
+//   var tag = document.getElementById('tagnames');
+//
+//
+//   var html_question_inner = ''
+//   if(html_question)
+//     html_question_inner = html_question.innerHTML
+//   else
+//     html_question = document.getElementById('wmd-preview')
+//   var date = new Date()
+//
+//   axios.post('http://127.0.0.1:5000/analyze',
+//   {
+//       "day": (date.getDay()).toString(),
+//       "hour": (date.getHours()).toString(),
+//       "body": html_question_inner,
+//       "title":title.value,
+//       "tags": tag.value.split(" ")
+//   })
+//     .then( (response) => {
+//
+//         percentage = parseInt(parseFloat(response.data.prediction)*100)
+//
+//     })
+//     .catch( (error) => {
+//       // console.log(error);
+//     });
+// }
